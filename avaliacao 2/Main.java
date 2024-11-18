68c0bd5ad80aac6559cbfbc269e3291aee63fae1
lautdssorri <142520436+lautdssorri@users.noreply.github.com>
2024-11-18 08:12:57 -0300

    Add files via upload

---
+++ avaliacao 2/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome: ");
+        String nome = scanner.nextLine();
+
+
+        pessoa pessoa = new pessoa();
+        pessoa.setNome(nome);
+        
+
+        System.out.println("Nome: " + pessoa.getNome());
+    }      
+}
